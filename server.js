e021018fade92e897b8a49a1275acebf89b9d918
utkrshydv <utkarsh4socials@gmail.com>
2025-05-24 19:45:21 +0530

    hopefully final commit

--- server.js
+++ server.js
@@ -1,30 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-require('dotenv').config();
+const bodyParser = require('body-parser');
 const db = require('./db');
+require('dotenv').config();
 
 app.use(express.json());
 
-let isReady = false;
-
-const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-  if (!isReady) {
-    return res.send('Waking up... Please wait.');
-  }
-  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+  res.sendFile(path.join(__dirname,'public','home.html')); 
 });
 
-// Wait for DB connection before enabling routes and starting server
-db.on('connected', () => {
-  isReady = true;
+const bookRoutes = require('./routes/bookRoutes');
 
-  const bookRoutes = require('./routes/bookRoutes');
-  app.use('/book', bookRoutes);
+app.use('/book', bookRoutes);
 
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
-});
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`)
+})
